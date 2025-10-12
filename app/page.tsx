d36b0ae9449ea51ffaddd6c5e73b11fead207b4b
toffly <toffly@outlook.com>
2025-10-12 12:31:37 +0700

    implement hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 const Page = () => {
     return (
         <div>
             <Navbar />
+            <Hero />
         </div>
     )
 }
