b27cdb7142fe12bfa703b38d352f2e0cd59bc9db
toffly <toffly@outlook.com>
2025-10-16 11:42:49 +0700

    show case section /animation not working

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Hero from "@/components/Hero";
 import ProductViewer from "@/components/ProductViewer";
 import gsap from "gsap";
 import {ScrollTrigger} from "gsap/all";
+import Showcase from "@/components/Showcase";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -13,6 +14,7 @@ const Page = () => {
             <Navbar />
             <Hero />
             <ProductViewer/>
+            <Showcase/>
         </div>
     )
 }
