e261df22424cbb2a21bb356bb961566e08216f1b
toffly <toffly@outlook.com>
2025-10-16 12:51:47 +0700

    implement performance section

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,29 @@
-import React from 'react'
+import React from "react";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import ProductViewer from "@/components/ProductViewer";
 import gsap from "gsap";
-import {ScrollTrigger} from "gsap/all";
+import { ScrollTrigger } from "gsap/all";
 import Showcase from "@/components/Showcase";
+import Performance from "@/components/Performance";
+import Features from "@/components/Features";
+import Highlights from "@/components/Highlights";
+import Footer from "@/components/Footer";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Page = () => {
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <ProductViewer/>
-            <Showcase/>
-        </div>
-    )
-}
-export default Page
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <ProductViewer />
+      <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
+    </div>
+  );
+};
+export default Page;
